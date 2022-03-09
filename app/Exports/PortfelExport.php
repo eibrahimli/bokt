4fbdef6ba27bbd80563add42d0a8165ecf7d570b
elvir <ibrahimlielvir@gmail.com>
2022-03-09 15:25:26 +0400

    Kredit portfel hesabati

--- app/Exports/PortfelExport.php
+++ app/Exports/PortfelExport.php
@@ -2,8 +2,10 @@
 
     namespace App\Exports;
 
-    use App\Models\Loan;
-    use Illuminate\Support\Collection;
+    use App\Helpers\LoanHelper;
+use App\Models\Loan;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
     use Maatwebsite\Excel\Concerns\FromCollection;
     use Maatwebsite\Excel\Concerns\FromQuery;
     use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -11,7 +13,7 @@
     use Maatwebsite\Excel\Concerns\WithMapping;
 
     class PortfelExport implements FromCollection,WithHeadings,ShouldAutoSize
-    {    
+    {
         protected $loans;
 
         function __construct($loans) {
@@ -19,7 +21,7 @@
 
             $this->reformatLoansForCorrectExport();
         }
-        
+
         public function collection()
         {
             return $this->loans;
@@ -30,7 +32,10 @@
             return [
                 'id', 'Müştəri','Müştəri nömrəsi', 'Kredit Əməkdaşı',
                 'Iqtisadi Rayon','Maliyyə mənbəyi','Kredit Məhsulunun adı',
-                'Götürdüyü Məbləğ','Maraq Faizi'
+                'Götürdüyü Məbləğ','Maraq Faizi', 'Bitmə tarixi', 'Xidmət haqqı','Qalıq Əsas Məbləğ',
+                'Qalıq Faiz Məbləği', 'Gözlənilən Cərimə','Cəm', 'Ödənilmiş Əsas Məbləğ','Ödənilmiş Faiz Məbləğ',
+                'Ödənilmiş Cərimə', 'Kreditin Götürülmə Tarixi','Kreditinin Müddəti', 'Cins','Ünvan',"Tel", "Fin",
+                "Ş/V Nömrəsi","Son Ödəniş Tarixi"
             ];
         }
 
@@ -38,6 +43,8 @@
             $this->loans = $this->loans->map(function($item) {
                 $data = [];
                 $customer = $item->customer;
+                $loanReports = $item->loanReports;
+                $activeLoanReports = $item->loanReports()->active()->get();
 
                 $data['id'] = $item->id;
                 $data['customer_name'] = $customer->name.' '. $customer->surname.' '. $customer->fathername;
@@ -47,11 +54,31 @@
                 $data['pay_from_where'] = '';
                 $data['product_name'] = $item->product->name;
                 $data['price'] = $item->price;
-                $data['interest_sum'] = $item->loanReports->sum('percentDept');
-
+                $data['interest_sum'] = $loanReports->sum('percentDept');
+                $data['ended_time'] = $loanReports->last()->shouldPay;
+                $data['service_fee'] = $loanReports->first()->service_fee;
+                $data['main_price_remainder'] = LoanHelper::findMainDept($item);
+                $data['percentage_price_remainder'] = LoanHelper::findPercentDept($item);
+                $data['penalty'] = $activeLoanReports->first()->penalty;
+                $data['total'] = round($data['service_fee'] + $data['main_price_remainder'] + $data['percentage_price_remainder'] + $data['penalty'],'2');
+                $data['payed_main'] = LoanHelper::findPayedMainAndPercent($item);
+                $data['payed_percentage'] = LoanHelper::findPayedMainAndPercent($item, 'interest');
+                $data['payed_penalty'] = LoanHelper::findPayedMainAndPercent($item, 'penalty');
+                $data['created_at'] = Carbon::parse($item->created_at)->toDateString();
+                $data['month'] = $item->month;
+                $data['gender'] = $customer->gender == 'male' ? 'Kişi' : 'Qadın';
+                $data['address'] = $customer->residential_address;
+                $data['contact_phones'] = $customer->contact_phone .','.$customer->contact_phone_1.','.$customer->contact_phone_2.','.$customer->contact_phone_3;
+                $data['fin'] = $customer->fin;
+                $data['identity_number'] = $customer->identity_number;
+                $data['last_payed_date'] = $item->transactions ? Carbon::parse($item->transactions->first()->created_at)->toDateString() : '';
                 return $data;
             });
-            
+
+        }
+
+        protected function isRescheduled(Loan $loan) {
+            return $loan->rescheduled;
         }
     }
 
--- app/Helpers/LoanHelper.php
+++ app/Helpers/LoanHelper.php
@@ -20,4 +20,42 @@ class LoanHelper
 
         return round($sum - $mainDeptRemainder, 2);
     }
+    public static function findPercentDept(Loan $loan): float
+    {
+        $reports = $loan->loanReports()->active()->get();
+
+        $sum = $reports->sum('percentDept');
+
+        $percentageRemainder = $reports->sum('percentage_remainder');
+
+        return round($sum - $percentageRemainder, 2);
+    }
+
+    public static function findPayedMainAndPercent(Loan $loan, $type = 'main')
+    {
+        $transactions = $loan->transactions;
+
+        if($transactions):
+            $sum = 0;
+
+            switch ($type) {
+                case 'main';
+                    $sum = $transactions->sum('main_price');
+                break;
+
+                case 'interest':
+                    $sum = $transactions->sum('interested_price');
+                break;
+
+                case 'penalty':
+                    $sum = $transactions->sum('calculated_price');
+                break;
+            }
+            return round($sum, '2');
+        else:
+
+            return 0;
+        endif;
+
+    }
 }
