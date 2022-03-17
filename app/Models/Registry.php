10ab1985c95eaf1df1c2c8897b2e29b6d32f49ad
Nesibli Yusif <Nesibli Yusif>
2022-03-17 19:52:48 +0400

    trial balance helli3

--- app/Models/Registry.php
+++ app/Models/Registry.php
@@ -122,7 +122,12 @@ class Registry extends Model
         }
 
         if(count($m_ids)>0){
-            $all = Registry::where("amount",">",0)->orWhereIn("debet",$m_ids)->orWhereIn("credit",$m_ids)->get();
+            $all = Registry::where("amount",">",0)
+                ->where(function ($query) use ($m_ids) {
+                    $query->orWhereIn("debet",$m_ids)->orWhereIn("credit",$m_ids);
+                })
+                ->get();
+            dd($all); 
             foreach ($all as $a){
                 $created_at = date("Y-m-d",strtotime($a->created_at));
                 if($created_at>=$begin and $created_at<=$end){
